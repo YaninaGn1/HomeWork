bcc375bb38b9b41b4d21eec25ea7da2bf0a30798
Yanina Hniazdouskaya <www23yanina@gmail.com>
2024-02-21 12:12:43 +0300

    В zadanie2 поставлено --с вместо с; в Zadanie6 добавлена конструкция if else для Анастасии

--- src/home_work_1/Zadanie6.java
+++ src/home_work_1/Zadanie6.java
@@ -9,16 +9,19 @@ public class Zadanie6 {
         String name1 = in.nextLine();
 
         String name = "Вася";
+        String name2 = "Анастасия";
 
         if (name.equals(name1)) {
             System.out.println("Привет");
             System.out.println("Я тебя так долго ждал");
+        } else if (name2.equals(name1)) {
+            System.out.println("Я тебя так долго ждал");
         } else {
             System.out.print("Добрый день, а вы кто?");
+
         }
     }
 }
-
 
 
 
